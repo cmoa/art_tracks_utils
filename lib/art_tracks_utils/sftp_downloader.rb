4bd4795914359812297d46283611291ecc2a9096
David Newbury <david.newbury@gmail.com>
2015-11-26 09:44:21 -0500

    Added teenie code

--- lib/art_tracks_utils/sftp_downloader.rb
+++ lib/art_tracks_utils/sftp_downloader.rb
@@ -30,12 +30,15 @@ module ArtTracksUtils
           if opts.type.nil? || opts.type == type.to_s
             if files[type].nil?
               files[type] = path
+              files[:teenie] = path if type == :thing
+              files[:accessioned] = path if type == :thing
             end
           end
         end
 
         # download those most recent files to tempfiles
         art = ArtTracks::ArtTracks.new({verbose: $stdout.tty?, internal_images:  false})
+        to_be_processed = {}
         files.each do |key,val|
           output_file = nil
           if opts.xml
@@ -43,8 +46,30 @@ module ArtTracksUtils
           else
             output_file = Tempfile.new(key.to_s)
           end
-          sftp.download!(val, output_file)
-          art.kemu_xml_to_json(output_file, nil, directory)
+          sftp.download!(val, output_file) do |event, downloader, *args|
+            case event
+            when :open then
+              # args[0] : file metadata
+              puts "starting download: #{args[0].remote} -> #{args[0].local} (#{args[0].size} bytes}"
+            when :get then
+              # args[0] : file metadata
+              # args[1] : byte offset in remote file
+              # args[2] : data that was received
+              puts "writing #{args[2].length} bytes to #{args[0].local} starting at #{args[1]}"
+            when :close then
+              # args[0] : file metadata
+              puts "finished with #{args[0].remote}"
+            when :mkdir then
+              # args[0] : local path name
+              puts "creating directory #{args[0]}"
+            when :finish then
+              puts "all done!"
+            end
+          end
+          to_be_processed[key] = output_file
+        end
+        to_be_processed.each do |key,output_file|
+          art.kemu_xml_to_json(output_file, nil, directory, {type: key})
         end
       end
     end
